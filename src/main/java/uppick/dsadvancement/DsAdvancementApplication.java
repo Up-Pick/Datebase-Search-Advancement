b163e98a9f21eef40ea1da52b499e6aaa88748be
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-10-22 10:31:50 +0900

    init project

---
+++ src/main/java/uppick/dsadvancement/DsAdvancementApplication.java
@@ -0,0 +1,13 @@
+package uppick.dsadvancement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsAdvancementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsAdvancementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uppick/dsadvancement/DsAdvancementApplicationTests.java
@@ -0,0 +1,13 @@
+package uppick.dsadvancement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsAdvancementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

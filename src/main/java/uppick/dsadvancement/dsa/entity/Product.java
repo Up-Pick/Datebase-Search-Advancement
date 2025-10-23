fc8ff67c2e9672ab13e223b171deff582e70a4ae
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-10-23 18:52:25 +0900

    feat: Product Getter 적용

--- src/main/java/uppick/dsadvancement/dsa/entity/Product.java
+++ src/main/java/uppick/dsadvancement/dsa/entity/Product.java
@@ -9,9 +9,11 @@ import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder

12fb0f000b2bcb0b0318e42d03679e697c6ed75b
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-10-23 18:54:57 +0900

    test: Elasticsearch 테스트 구현

--- src/test/java/uppick/dsadvancement/ProductServicePerformanceTest.java
+++ src/test/java/uppick/dsadvancement/ProductServicePerformanceTest.java
@@ -13,6 +13,8 @@ import lombok.extern.slf4j.Slf4j;
 import uppick.dsadvancement.dsa.Dto.response.ProductSearchDto;
 import uppick.dsadvancement.dsa.entity.Product;
 import uppick.dsadvancement.dsa.service.ProductService;
+import uppick.dsadvancement.search.document.ProductDocument;
+import uppick.dsadvancement.search.service.ProductSearchService;
 
 @Slf4j
 @SpringBootTest
@@ -21,8 +23,10 @@ public class ProductServicePerformanceTest {
 
 	@Autowired
 	private ProductService productService;
+	@Autowired
+	private ProductSearchService productSearchService;
 
-	private static final String KEYWORD = "123번이에요";
+	private static final String KEYWORD = "123번 신발";
 
 	@Test
 	void compareAveragePerformance() {
@@ -34,12 +38,16 @@ public class ProductServicePerformanceTest {
 		long jpaWithLike = 0;
 		long dslWithLike = 0;
 
+		long elasticWithQuery = 0;
+
 		for (int i = 0; i < iterations; i++) {
 			jpaWithFullText += measure(() -> productService.findProductsWithJpaByFullText(KEYWORD));
 			dslWithFullText += measure(() -> productService.findProductsWithDslByFullText(KEYWORD));
 
 			jpaWithLike += measure(() -> productService.findProductsWithJpaByLike(KEYWORD));
 			dslWithLike += measure(() -> productService.findProductsWithDslByLike(KEYWORD));
+
+			elasticWithQuery += measure(() -> productSearchService.findProductsWithElasticsearch(KEYWORD));
 		}
 
 		log.info("[JPA With Index Average] : {} ms", jpaWithFullText / iterations);
@@ -48,12 +56,17 @@ public class ProductServicePerformanceTest {
 		log.info("[JPA With No Index Average] : {} ms", jpaWithLike / iterations);
 		log.info("[DSL With No Index Average] : {} ms", dslWithLike / iterations);
 
+		log.info("[Elastic With Query] : {} ms", elasticWithQuery / iterations);
+
 		List<Product> p1 = productService.findProductsWithJpaByFullText(KEYWORD);
 		List<ProductSearchDto> p2 = productService.findProductsWithDslByFullText(KEYWORD);
 
 		List<Product> p3 = productService.findProductsWithJpaByLike(KEYWORD);
 		List<ProductSearchDto> p4 = productService.findProductsWithDslByLike(KEYWORD);
 
+		List<ProductDocument> p5 = productSearchService.findProductsWithElasticsearch(KEYWORD);
+
+		// List 디버깅
 		log.info("Test done");
 	}
 
